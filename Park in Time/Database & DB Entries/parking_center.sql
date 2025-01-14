28a8c23c8b9fafd6603bd430c774669b62f53342
SotosTz <t8220149@aueb.gr>
2025-01-15 00:24:07 +0200

    Add files via upload

---
+++ Park in Time/Database & DB Entries/parking_center.sql
@@ -0,0 +1,11 @@
+INSERT INTO Areas VALUES (201, 'Σύνταγμα', 1);
+INSERT INTO Areas VALUES (202, 'Μοναστηράκι', 1);
+INSERT INTO Areas VALUES (203, 'Ομόνοια', 1);
+INSERT INTO Areas VALUES (204, 'Κολωνάκι', 1);
+INSERT INTO Areas VALUES (205, 'Πλάκα', 1);
+
+INSERT INTO Parking VALUES ('201A', 'Parking Σύνταγμα', 'Πλατεία Συντάγματος 1', 3.00, 201, 100, '09:00:00', '21:00:00', 'https://maps.app.goo.gl/syntagmaParking');
+INSERT INTO Parking VALUES ('202A', 'Parking Μοναστηράκι', 'Αδριανού 15', 2.50, 202, 80, '08:00:00', '22:00:00', 'https://maps.app.goo.gl/monastirakiParking');
+INSERT INTO Parking VALUES ('203A', 'Parking Ομόνοια', 'Πλατεία Ομονοίας 10', 2.00, 203, 120, '07:00:00', '23:00:00', 'https://maps.app.goo.gl/omonoiaParking');
+INSERT INTO Parking VALUES ('204A', 'Parking Κολωνάκι', 'Σκουφά 50', 3.50, 204, 60, '08:00:00', '20:00:00', 'https://maps.app.goo.gl/kolonakiParking');
+INSERT INTO Parking VALUES ('205A', 'Parking Πλάκα', 'Λυσικράτους 5', 2.80, 205, 70, '09:00:00', '19:00:00', 'https://maps.app.goo.gl/plakaParking');
